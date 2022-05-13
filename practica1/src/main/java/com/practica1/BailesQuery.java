f3f287990fdef6d239026b00bdd4649561e4f6ac
javivu96 <94892691+javivu96@users.noreply.github.com>
2022-05-13 13:28:44 +0200

    Add files via upload

---
+++ practica1/src/main/java/com/practica1/BailesQuery.java
@@ -0,0 +1,54 @@
+
+package com.practica1;
+
+import org.exist.xmldb.EXistResource;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.XPathQueryService;
+
+
+public class BailesQuery {
+
+    private static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
+
+    /**
+     * args[0] Should be the name of the collection to access
+     * args[1] Should be the XQuery to execute
+     */
+    public static void main(String args[]) throws Exception {
+
+        final String driver = "org.exist.xmldb.DatabaseImpl";
+
+        // initialize database driver
+        Class cl = Class.forName(driver);
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+
+        Collection col = null;
+
+        try {
+            col = DatabaseManager.getCollection("xmldb:exist://localhost:8080/exist/xmlrpc/db/apps/Prueba");
+            XPathQueryService xpqs = (XPathQueryService)col.getService("XPathQueryService", "1.0");
+            xpqs.setProperty("indent", "yes");
+
+            ResourceSet result = xpqs.query(args["$bailes/baile/nombre"]);
+            ResourceIterator i = result.getIterator();
+            Resource res = null;
+            while(i.hasMoreResources()) {
+                try {
+                    res = i.nextResource();
+                    System.out.println(res.getContent());
+                } finally {
+                    //dont forget to cleanup resources
+                    try { ((EXistResource)res).freeResources(); } catch(XMLDBException xe) {xe.printStackTrace();}
+                }
+            }
+        } finally {
+            //dont forget to cleanup
+            if(col != null) {
+                try { col.close(); } catch(XMLDBException xe) {xe.printStackTrace();}
+            }
+        }
+    }
+}
